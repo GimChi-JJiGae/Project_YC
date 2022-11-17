59952642bcc39fb238089d40261cc7507ee33591
Gimchi-JJiGae <michaia0363@gmail.com>
2022-11-17 11:53:07 +0900

    22.11.17 11:53AM

--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -1,37 +1,36 @@
 <template>
   <div>
-    <img :src="this.top_movie_poster_list[8]" alt="">
-    <carousel-3d>
+    
+    <carousel-3d :width="400" :height="525">
     <slide :index="0">
-      {{ this.top_movie_poster_list[0] }}
+      <img :src="this.top_movie_poster_list[0]" alt="">
     </slide>
     <slide :index="1">
-      {{ this.top_movie_poster_list[1] }}
+      <img :src="this.top_movie_poster_list[1]" alt="">
     </slide>
     <slide :index="2">
-      {{ this.top_movie_poster_list[2] }}
+      <img :src="this.top_movie_poster_list[2]" alt="">
     </slide>
     <slide :index="3">
-      {{ this.top_movie_poster_list[3] }}
+      <img :src="this.top_movie_poster_list[3]" alt="">
     </slide>
     <slide :index="4">
-      {{ this.top_movie_poster_list[4] }}
+      <img :src="this.top_movie_poster_list[4]" alt="">
     </slide>
     <slide :index="5">
-      {{ this.top_movie_poster_list[5] }}
+      <img :src="this.top_movie_poster_list[5]" alt="">
     </slide>
     <slide :index="6">
-      {{ this.top_movie_poster_list[6] }}
+      <img :src="this.top_movie_poster_list[6]" alt="">
     </slide>
     <slide :index="7">
-      {{ this.top_movie_poster_list[7] }}
+      <img :src="this.top_movie_poster_list[7]" alt="">
     </slide>
     <slide :index="8">
-      
-      {{ this.top_movie_poster_list[8] }}
+      <img :src="this.top_movie_poster_list[8]" alt="">
     </slide>
     <slide :index="9">
-      {{ this.top_movie_poster_list[9] }}
+      <img :src="this.top_movie_poster_list[9]" alt="">
     </slide>
 
   </carousel-3d>
@@ -45,8 +44,16 @@ import { Carousel3d, Slide } from 'vue-carousel-3d'
 import _ from 'lodash'
 
 
+
+
 export default {
   name: 'HomeView',
+
+  data () {
+    return {
+      top_movie_poster_list: []
+    }
+  },
   computed: {
     isLogin() {
       return this.$store.getters.isLogin
@@ -59,8 +66,15 @@ export default {
   methods: {
     pick_top_poster_images: function () {
       for (let i = 0; i<10; i++) {
-        let number = _.range(0, 979)
-        this.top_movie_poster_list.push(this.$store.state.movies[number].poster_path)
+        let number = _.random(0, 979)
+        console.log(number)
+        console.log(this.$store.state.movies[number])
+        if (this.$store.state.movies[number].poster_path){ // 포스터 패스가 없을 경우는 다른걸 찾는다
+          this.top_movie_poster_list.push("https://image.tmdb.org/t/p/original/" + this.$store.state.movies[number].poster_path)
+        }
+        else {
+          i = i - 1
+        }
       }
     },
     getMovies: function () {
@@ -71,22 +85,27 @@ export default {
       }
       axios.get(`http://127.0.0.1:8000/movies/`)
       .then( (res) => {
+        
         if (this.$store.state.movies.length === 0) {
           this.$store.state.movies = res.data
           console.log(this.$store.state.movies)
         }
       })
+      
       .catch( (err) => {
         console.log(err)
       })
     },
   },
   created() {
-    if (this.$store.state.movies === []) {
+    console.log(this.$store.state.movies.length)
+    if (this.$store.state.movies.length === 0) {
       this.getMovies()
+      console.log('생성됨')
     }
-    this.pick_top_poster_images()
-    console.log(this.top_movie_poster_list)
+    console.log(this.$store.state.movies)
+    
+    
   },
 }
 </script>
