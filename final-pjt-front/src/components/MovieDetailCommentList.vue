359ae7e488b413289fae4db84ab71fa15fd1df57
michaia0363 <michaia0363@gmail.com>
2022-11-22 02:02:22 +0900

    22.11.22 2:02 AM movie_comment restart

---
+++ final-pjt-front/src/components/MovieDetailCommentList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <hr>
+    여기에 댓글목록 생성됨
+    <div class="row">
+      <div class="col-2">유저</div>
+      <div class="col-8">내용</div>
+      <div class="col-1"><font-awesome-icon icon="fa-regular fa-thumbs-up"/></div>
+      <div class="col-1"><font-awesome-icon icon="fa-regular fa-thumbs-down"/></div>
+    </div>
+    <hr>
+    <div class="comment-list">
+      <MovieDetailCommentListItem
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieDetailCommentListItem from './MovieDetailCommentListItem.vue';
+export default {
+  name: 'MovieDetailCommentList',
+  components: {
+    MovieDetailCommentListItem,
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>
